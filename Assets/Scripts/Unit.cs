ebcf64f4044f21c5153254bf6e8278dcbb43c2f8
InsaneGoblin <amochi84@gmail.com>
2024-10-24 17:28:52 +0200

    Step 16

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -18,15 +18,15 @@ public class Unit : MonoBehaviour
             transform.position += moveDirection * Time.deltaTime * moveSpeed;
         }
 
-        if (Input.GetKeyDown(KeyCode.T))
+        if (Input.GetMouseButtonDown(0))
         {
-            Move(new Vector3(4,0,4));
+            Move(targetPosition);
         }
     }
 
     private void Move(Vector3 targetPosition)
     {
-        this.targetPosition = targetPosition;
+        this.targetPosition = MouseWorld.GetPosition();
     }
 
 }
